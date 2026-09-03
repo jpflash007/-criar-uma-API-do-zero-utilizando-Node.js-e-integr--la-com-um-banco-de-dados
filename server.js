7471a8b7464526c9725d83002c9aed8cd33004cc
joao <joaoalmeida.ja617@gmail.com>
2026-09-03 14:21:28 -0300

    finalização da primara parte, esperando a integração do banco de dados

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json());
+
+const user= []
+
+//1-cria os usuários cadastrados
+
+app.post('/usuarios', (req, res) => {
+  user.push(req.body);
+  res.status(201).send('Usuário adicionado com sucesso!');
+});
+
+//2-consulta os usuários cadastrados
+
+app.get('/usuarios', (req, res) => {
+  res.status(200).json(user);
+ });
+
+app.listen(3000)
+
